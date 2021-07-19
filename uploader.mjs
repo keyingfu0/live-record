6a256f974a86d65cc6434d51ad47e1a77e5bbb7b
Cates Key <461354358@qq.com>
2021-07-19 21:46:18 +0800

    style

--- uploader.mjs
+++ uploader.mjs
@@ -8,25 +8,19 @@ let currentUploader = null
 const isFree = ref(true)
 
 effect(() => {
+  //  ? 为什么这里taskList的length还是1???? 不能用length判断了
   if (isFree.value && taskList.value.shift()) {
-    console.log(
-      '======================triggered================================='
-    )
-
     logger.log('info', {
-      message: '开始上传',
+      message: '==================开始上传==================',
     })
 
     isFree.value = false
 
     currentUploader = child_process.spawn('py', ['-3', 'uploader.py'])
-
     currentUploader.stdout.on('data', function (uploaderMessage) {})
-
     currentUploader.stderr.on('data', function (data) {
       console.log('stderr: ' + data)
     })
-
     currentUploader.on('close', function (code) {
       logger.log('info', {
         message: '上传程序结束',
